852b36adc0e485f11ae0c2d1e2e205c0973c4b59
Sidharth Mishra <sidharth1604@gmail.com>
2023-05-20 21:17:04 +0530

    Files Added

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,14 @@
 import express from 'express';
+const connectToMongo = require('./db');
+var cors = require('cors');
+require("dotenv").config();
+
+connectToMongo();
+
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.listen(8800, () => {
     console.log('Backend server is running!');

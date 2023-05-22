c485e255f064c027d00e153d8e608f4ba9393f07
Sidharth Mishra <sidharth1604@gmail.com>
2023-05-22 12:31:42 +0530

    Files Added

--- Backend/index.js
+++ Backend/index.js
@@ -30,6 +30,17 @@ app.use("/api/users", userRoute)
 app.use("/api/guesthouse", guesthouseRoute)
 app.use("/api/rooms", roomsRoute)
 
+app.use((err, req, res, next) => {
+    const errorStatus = err.status || 500;
+    const errorMessage = err.message || "Something went wrong!";
+    return res.status(errorStatus).json({
+      success: false,
+      status: errorStatus,
+      message: errorMessage,
+      stack: err.stack,
+    });
+  });
+
 app.listen(8800, () => {
     connect();
     console.log('Backend server is running!');
--- Backend/routes/guesthouse.js
+++ Backend/routes/guesthouse.js
@@ -13,4 +13,41 @@ router.post("/", async (req, res) => {
         res.status(500).json(err);
     }
 });
+//update
+router.put("/:id", async (req, res) => {
+    try {
+        const updatedGuesthouse = await Guesthouse.findByIdAndUpdate(req.params.id,{ $set: req.body}, {new: true});
+        res.status(200).json(updatedGuesthouse);
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
+//delete
+router.delete("/:id", async(req, res)=>{
+    try{
+        await Guesthouse.findByIdAndDelete(req.params.id);
+        res.status(200).json("Guesthouse has been deleted...");
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
+//get
+router.get("/:id", async(req, res)=>{
+    try{
+        const guesthouse = await Guesthouse.findById(req.params.id);
+        res.status(200).json(guesthouse);
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
+//get all
+router.get("/", async(req, res, next)=>{
+    try{
+        const guesthouses = await Guesthouse.find();
+        res.status(200).json(guesthouses);
+    } catch(err) {
+        next(err);
+    }
+});
+
 export default router

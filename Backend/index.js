5695ab561165303ab48e99f483185784e286fb46
Sidharth Mishra <sidharth1604@gmail.com>
2023-05-22 10:38:56 +0530

    Changes Added

--- Backend/index.js
+++ Backend/index.js
@@ -1,15 +1,34 @@
 import express from 'express';
-const connectToMongo = require('./db');
-var cors = require('cors');
-require("dotenv").config();
-
-connectToMongo();
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import authRoute from './routes/auth.js';
+import userRoute from './routes/users.js';
+import guesthouseRoute from './routes/guesthouse.js';
+import roomsRoute from './routes/rooms.js';
 
 const app = express();
+dotenv.config();
 
-app.use(cors());
-app.use(express.json());
+const connect = async () =>{
+    try{
+        await mongoose.connect(process.env.MONGO);
+        console.log('Successfully connected to MongoDB!')
+    } catch (error) {
+        throw error;
+    }
+};
+
+mongoose.connection.on("disconnected", () => {
+    console.log("mongoDB disconnected!");
+});
+
+//middleware
+app.use("/auth", authRoute)
+app.use("/users", userRoute)
+app.use("/guesthouse", guesthouseRoute)
+app.use("/rooms", roomsRoute)
 
 app.listen(8800, () => {
+    connect();
     console.log('Backend server is running!');
 });

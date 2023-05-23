310529d82954be25512c1edbfcaf8802f90400bf
Sidharth Mishra <sidharth1604@gmail.com>
2023-05-23 12:19:57 +0530

    Route Added

--- Backend/routes/rooms.js
+++ Backend/routes/rooms.js
@@ -1,5 +1,21 @@
 import express from "express";
+import { verifyAdmin } from "../utils/verifyToken.js";
+import { createRoom, updateRoom, deleteRoom, getRoom, getAllRoom } from "../controllers/rooms_controller.js";
 
 const router = express.Router();
+//Create
+router.post("/:guestId", verifyAdmin, createRoom);
+
+//Update
+router.put("/:id", verifyAdmin, updateRoom);
+
+//Delete
+router.delete("/:id", verifyAdmin, deleteRoom);
+
+//Get
+router.get("/:id", verifyAdmin, getRoom);
+
+//GetAll
+router.get("/", verifyAdmin, getAllRoom);
 
 export default router

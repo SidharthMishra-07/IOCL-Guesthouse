3d45985891f8163e87568c995bfe1a4b094f72d7
Sidharth Mishra <sidharth1604@gmail.com>
2023-05-23 12:04:58 +0530

    Changes Added

--- Backend/routes/guesthouse.js
+++ Backend/routes/guesthouse.js
@@ -1,17 +1,18 @@
 import express from "express";
 import Guesthouse from "../models/Guest.js";
 import { createGuesthouse, deleteGuesthouse, getAllGuesthouse, getGuesthouse, updateGuesthouse } from "../controllers/guesthouse_controller.js";
+import {verifyAdmin} from "../utils/verifyToken.js"
 
 const router = express.Router();
 
 //create
-router.post("/", createGuesthouse);
+router.post("/", verifyAdmin, createGuesthouse);
 
 //update
-router.put("/:id", updateGuesthouse);
+router.put("/:id", verifyAdmin, updateGuesthouse);
 
 //delete
-router.delete("/:id", deleteGuesthouse);
+router.delete("/:id", verifyAdmin, deleteGuesthouse);
 
 //get
 router.get("/:id", getGuesthouse);
--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -1,18 +1,19 @@
 import express from "express";
 import { deleteUser, getAllUsers, getUser, updateUser } from "../controllers/user_controller.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
 //update
-router.put("/:id", updateUser);
+router.put("/:id", verifyUser, updateUser);
 
 //delete
-router.delete("/:id", deleteUser);
+router.delete("/:id", verifyUser, deleteUser);
 
 //get
-router.get("/:id", getUser);
+router.get("/:id", verifyUser, getUser);
 
 //get all
-router.get("/", getAllUsers);
+router.get("/", verifyAdmin, getAllUsers);
 
 export default router;

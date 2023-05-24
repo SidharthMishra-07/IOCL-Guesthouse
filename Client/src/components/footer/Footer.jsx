9827943595f4767904769cb183794caab515a87a
Sidharth Mishra <sidharth1604@gmail.com>
2023-05-24 13:40:59 +0530

    UI Changes

--- Client/src/components/footer/Footer.jsx
+++ Client/src/components/footer/Footer.jsx
@@ -5,46 +5,19 @@ const Footer = () => {
     <div className="footer">
       <div className="fLists">
         <ul className="fList">
-          <li className="fListItem">Countries</li>
-          <li className="fListItem">Regions</li>
-          <li className="fListItem">Cities</li>
-          <li className="fListItem">Districts</li>
-          <li className="fListItem">Airports</li>
-          <li className="fListItem">Hotels</li>
+          <li className="fListItem">Copyright &copy; Indian Oil Corporation Limited</li>
+          <li className="fListItem">Site Map | Terms of Use | Data Privacy Policy</li>
+          <li className="fListItem">| Hyperlinking Policy</li>
         </ul>
         <ul className="fList">
-          <li className="fListItem">Homes </li>
-          <li className="fListItem">Apartments </li>
-          <li className="fListItem">Resorts </li>
-          <li className="fListItem">Villas</li>
-          <li className="fListItem">Hostels</li>
-          <li className="fListItem">Guest houses</li>
+          <li className="fListItem">Toll FREE Number: 1800-2333-555</li>
+          <li className="fListItem">Commercial LPG Helpline No. 1860-5991-111</li>
         </ul>
         <ul className="fList">
-          <li className="fListItem">Unique places to stay </li>
-          <li className="fListItem">Reviews</li>
-          <li className="fListItem">Unpacked: Travel articles </li>
-          <li className="fListItem">Travel communities </li>
-          <li className="fListItem">Seasonal and holiday deals </li>
-        </ul>
-        <ul className="fList">
-          <li className="fListItem">Car rental </li>
-          <li className="fListItem">Flight Finder</li>
-          <li className="fListItem">Restaurant reservations </li>
-          <li className="fListItem">Travel Agents </li>
-        </ul>
-        <ul className="fList">
-          <li className="fListItem">Curtomer Service</li>
-          <li className="fListItem">Partner Help</li>
-          <li className="fListItem">Careers</li>
-          <li className="fListItem">Sustainability</li>
-          <li className="fListItem">Press center</li>
-          <li className="fListItem">Safety Resource Center</li>
-          <li className="fListItem">Investor relations</li>
-          <li className="fListItem">Terms & conditions</li>
+          <li className="fListItem">LPG Emergency Helpline: 1906</li>
+          <li className="fListItem"> <a href="https://www.planetecomsolutions.com/" target="_blank">Powered By PECS</a></li>
         </ul>
       </div>
-      <div className="fText">Copyright © 2022 Lamabooking.</div>
     </div>
   );
 };
--- Client/src/components/navbar/Navbar.jsx
+++ Client/src/components/navbar/Navbar.jsx
@@ -1,10 +1,12 @@
 import "./navbar.css"
+import logo from "../../asset/logo.gif"
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navContainer">
-        <span className="logo">lamabooking</span>
+        {/* <span className="logo">Indian Oil &nbsp;</span> */}
+        <img src={logo} alt="Logo"/>
         <div className="navItems">
           <button className="navButton">Register</button>
           <button className="navButton">Login</button>

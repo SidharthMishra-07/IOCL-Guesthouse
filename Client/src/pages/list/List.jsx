44052de84c1aaaf43a51208432e5faecaaff7c6f
Sidharth Mishra <sidharth1604@gmail.com>
2023-05-27 16:23:09 +0530

    Context API Added

--- Client/src/pages/list/List.jsx
+++ Client/src/pages/list/List.jsx
@@ -12,11 +12,15 @@ import Spinner from "../../components/loading/Spinner";
 const List = () => {
   const location = useLocation();
   const [destination, setDestination] = useState(location.state.destination);
-  const [date, setDate] = useState(location.state.date);
+  const [dates, setDates] = useState(location.state.dates);
   const [openDate, setOpenDate] = useState(false);
   const [options, setOptions] = useState(location.state.options);
 
   const {data, loading, error, reFetch} = useFetch(`/guesthouse?city=${destination}`);
+
+  const handleSearch = () => {
+    reFetch();
+  };
 
   return (
     <div>
@@ -33,14 +37,14 @@ const List = () => {
             <div className="lsItem">
               <label>Check-in Date</label>
               <span onClick={() => setOpenDate(!openDate)}>{`${format(
-                date[0].startDate,
+                dates[0].startDate,
                 "dd/MM/yyyy"
-              )} to ${format(date[0].endDate, "dd/MM/yyyy")}`}</span>
+              )} to ${format(dates[0].endDate, "dd/MM/yyyy")}`}</span>
               {openDate && (
                 <DateRange
-                  onChange={(item) => setDate([item.selection])}
+                  onChange={(item) => setDates([item.selection])}
                   minDate={new Date()}
-                  ranges={date}
+                  ranges={dates}
                 />
               )}
             </div>
@@ -88,7 +92,7 @@ const List = () => {
                 </div>
               </div>
             </div>
-            <button>Search</button>
+            <button onClick={handleSearch}>Search</button>
           </div>
           <div className="listResult">
             {loading ? (

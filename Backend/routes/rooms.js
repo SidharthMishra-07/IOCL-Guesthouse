3dbedaeefe962066b5825ad4f176d9c27863f7ac
Sidharth Mishra <sidharth1604@gmail.com>
2023-06-03 13:32:51 +0530

    Reserve Component complete

--- Backend/routes/rooms.js
+++ Backend/routes/rooms.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { verifyAdmin } from "../utils/verifyToken.js";
-import { createRoom, updateRoom, deleteRoom, getRoom, getAllRoom } from "../controllers/rooms_controller.js";
+import { createRoom, updateRoom, deleteRoom, getRoom, getAllRoom, updateRoomAvailability } from "../controllers/rooms_controller.js";
 
 const router = express.Router();
 //Create
 router.post("/:guestId", verifyAdmin, createRoom);
 
 //Update
+router.put("/availability/:id", updateRoomAvailability);
 router.put("/:id", verifyAdmin, updateRoom);
 
 //Delete
--- Client/src/components/reserve/Reserve.jsx
+++ Client/src/components/reserve/Reserve.jsx
@@ -10,7 +10,34 @@ import { useNavigate } from "react-router-dom";
 
 const Reserve = ({setOpen, guesthouseId}) => {
     const [selectedRooms, setSelectedRooms] = useState([]);
+    const { dates } = useContext(SearchContext);
 
+    const getDatesInRange = (startDate, endDate) => {
+      const start = new Date(startDate);
+      const end = new Date(endDate);
+  
+      const date = new Date(start.getTime());
+  
+      const dates = [];
+  
+      while (date <= end) {
+        dates.push(new Date(date).getTime());
+        date.setDate(date.getDate() + 1);
+      }
+  
+      return dates;
+    };
+  
+    const alldates = getDatesInRange(dates[0].startDate, dates[0].endDate);
+  
+    const isAvailable = (roomNumber) => {
+        const isFound = roomNumber.unavailableDates.some((date) =>
+        alldates.includes(new Date(date).getTime())
+      );
+  
+      return !isFound;
+    };
+  
     const handleSelect = (e) => {
         const checked = e.target.checked;
         const value = e.target.value;
@@ -20,9 +47,26 @@ const Reserve = ({setOpen, guesthouseId}) => {
                 : selectedRooms.filter((item) => item !== value)
         );
     };
-    const handleClick = () => {
-        
-    }
+    
+    const navigate = useNavigate();
+
+    const handleClick = async () => {
+        try {
+          await Promise.all(
+            selectedRooms.map((roomId) => {
+              const res = axios.put(`/rooms/availability/${roomId}`, {
+                dates: alldates,
+              });
+              return res.data;
+            })
+          );
+          setOpen(false);
+          navigate("/");
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
     console.log(guesthouseId);
     const { data, loading, error } = useFetch(`/guesthouse/room/${guesthouseId}`);
     return (
@@ -51,7 +95,7 @@ const Reserve = ({setOpen, guesthouseId}) => {
                                         type="checkbox"
                                         value={roomNumber._id}
                                         onChange={handleSelect}
-                                        // disabled={!isAvailable(roomNumber)}
+                                        disabled={!isAvailable(roomNumber)}
                                     />
                                 </div>
                             ))}

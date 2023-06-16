c9467e8af1f45565ad88b13d25f014dabe1d3a4c
Sidharth Mishra <sidharth1604@gmail.com>
2023-06-16 23:13:12 +0530

    Change Added

--- Client/src/components/searchItem/SearchItem.jsx
+++ Client/src/components/searchItem/SearchItem.jsx
@@ -28,15 +28,15 @@ const SearchItem = ({item, startDate, endDate}) => {
       <div className="siDesc">
         <h1 className="siTitle">{item.name}</h1>
         <span className="siDistance">{item.distance}m from center</span>
-        <span className="siTaxiOp">Free airport taxi</span>
+        <span className="siTaxiOp">Near to airport</span>
         <span className="siSubtitle">
           Studio Apartment with Air conditioning
         </span>
         <span className="siFeatures">{item.desc}</span>
-        <span className="siCancelOp">Free cancellation </span>
+        {/* <span className="siCancelOp">Free cancellation </span>
         <span className="siCancelOpSubtitle">
           You can cancel later, so lock in this great price today!
-        </span>
+        </span> */}
       </div>
       <div className="siDetails">
         {item.rating && <div className="siRating">
